5cab030b486e9ad33995e130acd2498dc4e980cf
Mateusz Luszczewski <mateusz.luszczewski@grupawp.pl>
2020-01-18 14:26:45 +0100

    Controllers

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using tech_dot_net_Mluszczewski.Data;
+using tech_dot_net_Mluszczewski.Models;
+
+namespace tech_dot_net_Mluszczewski.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        [Authorize(Roles = "User,Moderator,Admin")]
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var books = _context
+                .Books
+                .Include(b => b.Author)
+                .Include(b => b.Category)
+                .Where(s => (s.Title.Contains(searchString) || s.Description.Contains(searchString) ||
+                s.Author.FirstName.Contains(searchString) || s.Author.LastName.Contains(searchString))
+                || s.Category.Name.Contains(searchString) || s.Category.Description.Contains(searchString));
+            if (String.IsNullOrEmpty(searchString))
+            {
+                books = _context.Books.Include(b => b.Author).Include(b => b.Category);
+            }
+
+            return View(await books.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        [Authorize(Roles = "User,Moderator,Admin")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            ViewData["AuthorID"] = new SelectList(_context.Authors, "ID", "LastName");
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "ID", "Name");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create([Bind("ID,Title,Description,Image,PagesNumber,ReleasteDate,AuthorID,CategoryID")] Book book, IFormFile fileImage)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (fileImage != null)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = fileImage.OpenReadStream())
+                    using (var ms1 = new MemoryStream())
+                    {
+                        fs1.CopyTo(ms1);
+                        p1 = ms1.ToArray();
+                    }
+                    book.Image = p1;
+                }
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorID"] = new SelectList(_context.Authors, "ID", "LastName", book.AuthorID);
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "ID", "Name", book.CategoryID);
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        [Authorize(Roles = "Moderator,Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorID"] = new SelectList(_context.Authors, "ID", "LastName", book.AuthorID);
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "ID", "Name", book.CategoryID);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Moderator,Admin")]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Description,Image,PagesNumber,ReleasteDate,AuthorID,CategoryID")] Book book, IFormFile fileImage)
+        {
+            if (id != book.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (fileImage != null)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = fileImage.OpenReadStream())
+                    using (var ms1 = new MemoryStream())
+                    {
+                        fs1.CopyTo(ms1);
+                        p1 = ms1.ToArray();
+                    }
+                    book.Image = p1;
+                }
+
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorID"] = new SelectList(_context.Authors, "ID", "LastName", book.AuthorID);
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "ID", "Name", book.CategoryID);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.ID == id);
+        }
+    }
+}
