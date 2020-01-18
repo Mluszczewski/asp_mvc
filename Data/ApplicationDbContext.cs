9fbf47444f16b1a9d7a9ce390605e8cb8a1f37d6
Mateusz Luszczewski <mateusz.luszczewski@grupawp.pl>
2020-01-18 14:26:53 +0100

    Data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using tech_dot_net_Mluszczewski.Models;
+
+namespace tech_dot_net_Mluszczewski.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
